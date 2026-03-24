c3137d3465252a40813912aa5e8625d31ab273f5
Matik <matipol1665@gmail.com>
2026-03-24 07:57:02 +0100

    added sd_tasks (packer and writer)

--- app/include/sd_task.h
+++ app/include/sd_task.h
@@ -4,4 +4,24 @@
  * Date: 23.03.2026
  */
 #pragma once
-#include "ff.h"
+
+#include "main.h"
+#include "queue.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include "FreeRTOS.h"
+#include "board_data.h"
+
+#define LOG_FILE_NAME "data.csv"
+#define SD_QUEUE_LENGTH 50
+#define SD_BUFFER_BYTES 4096
+
+HAL_StatusTypeDef sd_logger_init(void);
+HAL_StatusTypeDef sd_logger_log_data(const BoardData_t *data);
+
+// NOT THREAD SAFE - use ONLY in sd_task
+bool sd_logger_mount(void);
+void sd_logger_unmount(void);
+bool sd_logger_remount(void);
+void sd_logger_sync(void);
+bool sd_logger_is_mounted(void);
